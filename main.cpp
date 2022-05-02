37f405c40a91b11a36b75818f458b899208c102b
KimNamWook <7922378@naver.com>
2022-05-02 09:17:14 +0900

    제출

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int MyGold = 10;
+
+	cout << MyGold + 10 << endl;
+	cout << MyGold - 10 << endl;
+	cout << MyGold * 10 << endl;
+	cout << MyGold / 10 << endl;
+	cout << MyGold % 10 << endl;
+
+	float G = 0.1f;
+
+	cout << G + 0.1f << endl;
+	cout << G - 0.1f << endl;
+	cout << G * 0.1f << endl;
+	cout << G / 0.1f << endl; 
+	//%연산자는 정수형에서만 사용 가능
+}
